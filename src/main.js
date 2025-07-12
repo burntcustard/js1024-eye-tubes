576fd3010c6e7bcf354a541a0b2169eee82c82ff
burntcustard <burntcustard@gmail.com>
2025-07-12 14:29:20 +0100

    Minor code golfing bits

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const tubes = [{},{},{}]; // Make tubes global so renderAllEyes can access it
-const tubeBorderWidth = 2; // Also Chrome/Firefox button default border width
+const tubeBorderWidth = 2;
 const eyeGap = 1;
 const eyeSize = 28;
 const tubeSize = 32;
@@ -16,26 +16,27 @@ let gameStarted;
 // ... but some might have 3 layers.
 const eyeTypes = [
   // human
-  `radial-gradient(#000 25%, #37f 30% 45%, #fee 50%)`,
+  `#000 25%, #37f 30% 45%, #fee 50%`,
   // feline
-  `radial-gradient(25% 65%, #000 50%, #1e3 60%`,
+  `25% 65%, #000 50%, #1e3 60%`,
   // reptile
-  `radial-gradient(32% 15%, #000 50%, #fa0 60%)`,
+  `32% 15%, #000 50%, #fa0 60%`,
   // owl
-  `radial-gradient(#000 25%, #fe0 30%)`,
+  `#000 25%, #fe0 30%`,
   // spider
-  `radial-gradient(22% 10% at 50% 24%, #eee 40%, #000 50%)`,
+  `22% 10% at 50% 24%, #eee 40%, #000 50%`,
 ];
 
 const createEye = (eyeTypeIndex) => {
   const eye = document.createElement('div');
   // position absolute makes more sense but fixed is fewer characters
   eye.style.position = 'fixed';
-  eye.style.width = `${eyeSize}px`;
   eye.style.height = `${eyeSize}px`;
+  eye.style.width = `${eyeSize}px`;
   eye.style.borderRadius = `${eyeSize}px`;
   eye.style.transition = 'all.2s';
-  eye.style.background = eyeTypes[eyeTypeIndex];
+  // No closing bracket is needed at end of radial-gradient, saves 2B
+  eye.style.background = `radial-gradient(${eyeTypes[eyeTypeIndex]}`;
   b.append(eye); // Immediately append eye to body
   return eye;
 }
@@ -103,8 +104,9 @@ const startGame = () => {
     tubeObject.tubeElement?.remove();
   });
 
+  // Fill each tube with eyes, except the 1st tube which is skip (its initially empty)
   tubes.forEach((tubeObject, tubeIndex) => {
-    tubeObject.eyes = (tubeIndex < tubes.length - 1) ? [
+    tubeObject.eyes = tubeIndex ? [
       createEye((eyeTypeIndex + tubeIndex) % numOfEyeTypes),
       createEye((eyeTypeIndex + tubeIndex) % numOfEyeTypes),
       createEye((eyeTypeIndex + tubeIndex) % numOfEyeTypes),
@@ -123,9 +125,9 @@ const startGame = () => {
     tubeElement.style.borderTop = '0';
     tubeElement.style.borderRadius = `0 0 ${tubeSize}px ${tubeSize}px`;
     tubeElement.style.background = '#fff1';
-    tubeElement.style.width = `${tubeSize + tubeBorderWidth * 2}px`;
-    tubeElement.style.height = `${4 * (eyeSize + eyeGap) + tubeSize - eyeSize + tubeBorderWidth * 2}px`;
     tubeElement.style.position = 'fixed';
+    tubeElement.style.height = `${4 * (eyeSize + eyeGap) + tubeSize - eyeSize + tubeBorderWidth * 2}px`;
+    tubeElement.style.width = `${tubeSize + tubeBorderWidth * 2}px`;
 
     tubeObject.clickHandler = tubeElement.onclick = () => {
       if (floatingEye) {
@@ -154,11 +156,9 @@ const startGame = () => {
       if (
         // bitwise AND `&` works here because we only care about truthiness
         gameStarted &
-        // tubeIndex and eye are unused but help with compression
-        tubes.every((tubeObject, tubeIndex) =>
-          tubeObject.eyes.every((eyeElement, eyeIndex) =>
-            // Remove ? after [3] to save 1 byte but introduce console error
-            tubeObject.eyes[3]?.style.background === tubeObject.eyes[eyeIndex].style.background
+        tubes.every((tubeObject) =>
+          tubeObject.eyes.every((eyeElement) =>
+            tubeObject.eyes[3]?.style.background === eyeElement.style.background
           )
         )
       ) {
@@ -178,7 +178,7 @@ const startGame = () => {
 
   // Shuffle by clicking random tubes 1000+ times keeping going if there's a floating eye
   // The ugly loop reverseness with no afterthought helps with compression
-  for (let shuffle = 0; floatingEye || 1000 * tubes.length > shuffle++;) {
+  for (let shuffle = 1000 * tubes.length; floatingEye || shuffle--;) {
     tubes[Math.random() * tubes.length | 0].clickHandler();
   }
 
