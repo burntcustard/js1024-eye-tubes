f246e5413b913943d633f2cdf7111ccae03875cf
burntcustard <burntcustard@gmail.com>
2025-07-14 19:33:56 +0100

    Minify eye backgrounds properly again to save 1B

--- src/main.js
+++ src/main.js
@@ -43,13 +43,14 @@ const createEye = (eyeTypeIndex) => {
   // Radial gradients are mostly:
   // optional(x-stretch y-stretch), center-color center-size, outer-color outer-size
   // ... but some might have 3 layers.
-  eyeElement.style.background = `radial-gradient(${[
+  // Comments are on the left because of awkward custom minification during build
+  eyeElement.style.background = 'radial-gradient(' + [
     /* Feline (green)   */ `25% 65%, #000 50%, #1e3 60%`,
     /* Owl (yellow)     */ `#000 25%, #fe0 30%`,
     /* Reptile (orange) */ `32% 15%, #000 50%, #fa0 60%`,
     /* Human            */ `#000 25%, #37f 30% 45%, #fee 50%`,
     /* Spider (black)   */ `22% 10% at 50% 24%, #eee 40%, #000 50%`,
-  ][eyeTypeIndex % numberOfEyeTypes]})`;
+  ][eyeTypeIndex % numberOfEyeTypes] + `)`;
 
   // Immediately append eye to document body
   b.append(eyeElement);
